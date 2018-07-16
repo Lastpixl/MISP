7d8033b540f0bbbf46b57bcf3ba222c2867e89a8
Amaury Leroy <amaury.leroy@airbus.com>
2018-07-16 16:00:35 +0200

    Pass passAlong to _edit inside Pull function

--- app/Model/Event.php
+++ app/Model/Event.php
@@ -2999,7 +2999,7 @@ class Event extends AppModel
                     ));
                 }
                 // do the necessary actions to publish the event (email, upload,...)
-				if ((true != Configure::read('MISP.disablerestalert')) && (empty($server) || $server['Server']['publish_without_email'] == 0)) {
+                if ((true != Configure::read('MISP.disablerestalert')) && (empty($server) || $server['Server']['publish_without_email'] == 0)) {
                     $this->sendAlertEmailRouter($id, $user, $existingEvent['Event']['publish_timestamp']);
                 }
                 $this->publish($existingEvent['Event']['id']);
